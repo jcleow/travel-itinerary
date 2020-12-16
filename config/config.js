4cf34416eb9c77f91173850a72bfd200caf0ef5a
jcleow <jc.leow94@gmail.com>
2020-12-16 14:27:50 +0800

    First commit

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  development: {
+    username: '<YOUR_UNIX_USER_NAME>',
+    password: null,
+    // Use "_development" suffix to indicate DB is for development purposes
+    database: '<YOUR_DB_NAME>_development',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+};
